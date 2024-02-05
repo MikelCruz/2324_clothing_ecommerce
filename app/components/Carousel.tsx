c9e67f177aa78bf67863edfc3b027e2258712b2c
Jonathan Ramos <jonathan.ramos@ikasle.aeg.eus>
2024-02-05 20:25:36 +0100

    [ADD] Added Sanity Images to Carousel TODO: Carousel does not change image

--- app/components/Carousel.tsx
+++ app/components/Carousel.tsx
@@ -3,33 +3,56 @@ import React from 'react';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
-const Carousel = () => {
+import { client, urlFor } from "../lib/sanity";
+import Image from 'next/image';
+
+
+async function getData() {
+  const query = `*[_type == 'heroImage'][0]{
+    images
+  }`;
+
+  const data = await client.fetch(query);
+
+  return data;
+}
+
+const Carousel = async ()=> {
+
+  const data = await getData();
+
   const settings = {
     dots: true,
     infinite: true,
-    speed: 500,
+    speed: 100,
     slidesToShow: 1,
     slidesToScroll: 1,
   };
+
   return (
     <div className="">
-      <div style={{
+      <div 
+      style={{
         marginTop: '6%',
         marginBottom: '3',
         backgroundColor: 'rgba(232, 195, 158, 0.7)',
         borderRadius: '20px' }}
         className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+          
       <Slider {...settings}>
-        <div>
-          <img src="../../assets/image1.jpg" alt="Image 1" />
-        </div>
-        <div>
-          <img src="../../assets/image3.jpg" alt="Image 2" />
-        </div>
-        <div>
-          <img src="../../assets/image2.jpg" alt="Image 3" />
-        </div>
-        {/* Add more slides as needed */}
+        {data.images.map((image: any, index: any)=>{
+
+          return(
+            <div key={index}>
+             <Image
+              src={urlFor(image).url()}
+              width={200}
+              height={200}
+              alt="photo"
+            />
+            </div>
+          )
+        })}
       </Slider>
       </div>
     </div>
